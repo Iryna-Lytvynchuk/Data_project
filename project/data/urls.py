d9a96be6d78d24579522f9bdbfedc01707002be3
Ir <75753284+Iryna-Lytvynchuk@users.noreply.github.com>
2022-01-01 22:18:07 +0200

    new

--- project/data/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import HomePageView, CreateDataView
-
- 
-urlpatterns = [
-    path('', CreateDataView.as_view(), name='add_data'),
-    path('data/',HomePageView.as_view(), name='home') 
-]
